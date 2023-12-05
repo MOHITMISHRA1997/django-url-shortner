1f8aaee19dbe4dfa728d24b85e69dfc905bd2342
mohit <mohitmishra001.mm@gmail.com>
2023-12-05 23:23:13 +0530

    This is final commit

--- urlshortner/myapp/models.py
+++ urlshortner/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class URLmodel(models.Model):
+    sno = models.AutoField(primary_key=True)
+    orignal_url = models.URLField("URL", unique=False)
+    new_url = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.orignal_url
+    
--- urlshortner/myapp/views.py
+++ urlshortner/myapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import URLmodel
+import random
+import string
 
 # Create your views here.
+def home(request):
+
+    if request.method == 'POST':
+        yoururl = request.POST.get('orignal_url')
+        random_chars = string.ascii_letters  # This includes both lowercase and uppercase letters
+        shortened_url = ''.join(random.sample(random_chars, 7))
+
+        URLmodel.objects.create(orignal_url=yoururl,new_url=shortened_url)
+
+        shorturl = "http://localhost:8000/"+shortened_url
+        return render(request,'home.html',{'yoururl':yoururl,'shorturl':shorturl})
+
+    return render(request,'home.html')
+
+
+def orignalurlredirect(request,newurl):
+    url = URLmodel.objects.get(new_url=newurl)
+    print('This is :',url)
+    return redirect(url.orignal_url)
